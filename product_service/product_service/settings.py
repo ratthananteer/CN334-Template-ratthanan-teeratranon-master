dfb03d4654282243f4ff793cc191de935a5f42f5
ratthananteer <ratthanan.2560@gmail.com>
2025-05-15 23:19:52 +0700

    da

--- product_service/product_service/settings.py
+++ product_service/product_service/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'product_management',
-    'user_management',
     'rest_framework',
     'corsheaders',
     'rest_framework_simplejwt',
